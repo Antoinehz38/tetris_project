7e9f4fc9d0f549a791337b0d75791bd9d4e8fb89
antoine <antoine.hanachowicz@outlook.com>
2026-01-19 10:05:40 +0100

    training implem

---
+++ src/view_play.py
@@ -0,0 +1,32 @@
+import cv2
+import gymnasium as gym
+from gymnasium.wrappers import FlattenObservation
+from tetris_gymnasium.envs.tetris import Tetris
+from stable_baselines3 import DQN
+
+if __name__ == "__main__":
+    env = gym.make(
+        "tetris_gymnasium/Tetris",
+        render_mode="human",
+        render_upscale=40
+    )
+    env = FlattenObservation(env)   
+
+    observation, info = env.reset()
+
+    model = DQN.load("runs/tetris/dqn_tetris_final.zip")
+
+    terminated = False
+    total_reward = 0
+
+    while not terminated:
+        env.render()
+        action, _ = model.predict(observation, deterministic=True)
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        key = cv2.waitKey(1)
+        total_reward += reward
+        print("Score:", total_reward, end="\r", flush=True)
+
+    print("\nGame Over!")
+    print("Final Score:", total_reward)
